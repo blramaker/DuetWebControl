cb08b8077c5b41235a5750dd57fcfb193862708b
blramaker <76038297+blramaker@users.noreply.github.com>
2021-03-22 00:04:11 +0800

    Update zh_cn.js

--- src/i18n/zh_cn.js
+++ src/i18n/zh_cn.js
@@ -1,5 +1,3 @@
-// 注释：更新到2.1.7，实际版本为2.1.3
-// 注释：https://github.com/ningjiazun
 export default {
 	language: '简体中文',
 	'$vuetify': {
@@ -68,16 +66,16 @@ export default {
 			title: '立即执行软件重启(M112+M999)'
 		},
 		home: {
-			caption: '归位{0}轴',
-			captionAll: '全部归位',
-			title: '归位{0}轴（G28 {0}）',
-			titleAll: '归位所有轴（G28）'
+			caption: '{0}轴归零',
+			captionAll: '全部归零',
+			title: '{0}轴归零（G28 {0}）',
+			titleAll: '所有轴归零（G28）'
 		},
 		newDirectory: {
 			caption: '新建目录'
 		},
 		newFilament: {
-			caption: '新建细丝'
+			caption: '新建耗材'
 		},
 		newFile: {
 			caption: '新建文件'
@@ -95,16 +93,16 @@ export default {
 				title: '上传一个或多个G-Code文件（支持拖放）'
 			},
 			start: {
-				caption: '上传并开始',
-				title: '上传和开始一个或多个G-Code文件（支持拖放）'
+				caption: '上传并打印',
+				title: '上传和开始打印一个或多个G-Code文件（支持拖放）'
 			},
 			macros: {
 				caption: '上传宏文件',
 				title: '上传一个或多个宏文件（支持拖放）'
 			},
 			filaments: {
-				caption: '上传细丝配置',
-				title: '上传一个或多个细丝配置（支持拖放）'
+				caption: '上传耗材配置',
+				title: '上传一个或多个耗材配置（支持拖放）'
 			},
 			menu: {
 				caption: '上传菜单文件',
@@ -135,12 +133,13 @@ export default {
 			layer: '层{0}',
 			layerDuration: '持续时间：{0}',
 			layerHeight: '层高度：{0}',
-			filamentUsage: '细丝用量：{0}',
+			filamentUsage: '耗材用量：{0}',
 			fractionPrinted: '文件进度：{0}'
 		},
 		temperature: {
 			caption: '温度图表',
 			heater: '加热器{0}',
+			sensor: '传感器{0}',
 			noData: '没有数据'
 		}
 	},
@@ -168,7 +167,7 @@ export default {
 			disconnecting: '正在断开连接...',
 			updating: '正在安装更新，请稍候...',
 			reconnecting: '连接丢失，尝试重新连接...',
-			standBy: '请等待...'
+			standBy: '请稍候...'
 		},
 		editExtrusionAmount: {
 			title: '编辑挤出量',
@@ -183,15 +182,29 @@ export default {
 			prompt: '您确定要执行恢复出厂设置吗？ 所有保存的设置都将丢失。'
 		},
 		filament: {
-			titleChange: '改变细丝',
-			titleLoad: '载入细丝',
-			prompt: '请选择细丝：'
+			titleChange: '改变耗材',
+			titleLoad: '载入耗材',
+			prompt: '请选择耗材：'
 		},
 		fileEdit: {
 			gcodeReference: 'G-Code参考',
 			menuReference: '菜单参考',
 			save: '保存',
 			confirmClose: '文件已修改。如果继续，您的修改将丢失。'
+		},
+		fileTransfer: {
+			uploadingTitle: '正在上传文件{0} 总共{1}个,已完成{2}%',
+			uploadDoneTitle: '上传成功！',
+			uploadFailedTitle: '上传失败！',
+			downloadingTitle: '正在下载文件{0} 总共{1}个,已完成{2}%',
+			downloadDoneTitle: '下载成功！',
+			downloadFailedTitle: '下载失败！',
+			filename: '文件名',
+			size: '大小',
+			progress: '进度',
+			currentSpeed: '当前速度：{0}',
+			cancelUploads: '取消上传',
+			cancelDownloads: '取消下载'
 		},
 		meshEdit: {
 			title: '设置网格参数',
@@ -206,8 +219,8 @@ export default {
 			prompt: '请输入新建目录的名称：'
 		},
 		newFilament: {
-			title: '新建细丝',
-			prompt: '请输入新建细丝的名称：'
+			title: '新建耗材',
+			prompt: '请输入新建耗材的名称：'
 		},
 		newFile: {
 			title: '新建文件',
@@ -219,7 +232,7 @@ export default {
 		},
 		resetHeaterFault: {
 			title: '重置加热器故障',
-		prompt: '加热器{0}发生加热器故障。强烈建议立即关闭机器并在继续之前检查接线。 如果您确认这不是硬件问题，您可以重置加热器故障，【自担风险】！请注意这是【不推荐】的，可能会导致进一步的问题。 你想怎样处理？',
+		prompt: '加热器{0}发生加热器故障。强烈建议立即关闭机器并在继续之前检查接线。 如果您确认这不是硬件问题，您可以重置加热器故障，【自担风险】！请注意【不推荐】这么做，可能会导致其他问题。 你想怎样处理？',
 			resetFault: '重置故障'
 		},
 		runMacro: {
@@ -232,18 +245,20 @@ export default {
 		},
 		update: {
 			title: '安装更新？',
-			prompt: '您已上传至少一个固件更新。你想现在安装吗？'
+			prompt: '您至少上传了一个固件更新。您想现在安装吗？',
+			resetTitle: '重置固件？',
+			resetPrompt: '您刚刚安装了扩展板更新。 是否要重新启动主控制器以恢复以前的配置？ '
 		},
 		inputRequired: '请输入一个值',
 		numberRequired: '请输入有效的数值'
 	},
 	directory: {
 		menu: '菜单目录',
-		filaments: '细丝目录',
-		gcodes: 'G-Codes目录',
+		filaments: '耗材设置',
+		gcodes: '切片文件',
 		macros: '宏目录',
-		system: '系统目录',
-		web: 'WWW目录'
+		system: '系统文件',
+		web: 'WWW文件'
 	},
 	error: {
 		notImplemented: '{0}没有实施',
@@ -263,14 +278,13 @@ export default {
 		uploadStartWrongFileCount: '只能上传和启动单个文件',
 		uploadNoSingleZIP: '一次只能上传一个ZIP文件',
 		uploadNoFiles: '此ZIP不包含任何可用文件',
-		uploadDecompressionFailed: '无法解压缩ZIP文件',
 		codeResponse: '无法运行代码，因为收到了错误的响应',
 		codeBuffer: '可以运行代码，因为缓冲区空间已用完',
 		enterValidNumber: '请输入有效的数值',
 		turnOffEverythingFailed: '无法关闭所有内容',
 		filelistRequestFailed: '无法获取文件列表',
 		fileinfoRequestFailed: '无法获取文件{0}的信息',
-		filamentsLoadFailed: '无法加载细丝',
+		filamentsLoadFailed: '无法加载耗材',
 		move: '从{0}移动到{1}失败'
 	},
 	events: {
@@ -288,7 +302,7 @@ export default {
 		no: '否',
 		close: '关闭',
 		reset: '重置',
-		noValue: '没有',
+		noValue: '无数据',
 		loading: '正在载入',
 		error: '错误',
 		info: '信息',
@@ -297,12 +311,13 @@ export default {
 		heaterStates: {
 			off: '关闭',
 			standby: '待机',
-			active: '活动',
+			active: '运行',
 			fault: '故障',
 			tuning: '正在调谐',
 			offline: '离线'
 		},
 		status: {
+			starting: '开始',
 			updating: '正在更新',
 			off: '关闭',
 			halted: '已停止',
@@ -313,7 +328,7 @@ export default {
 			processing: '正在处理',
 			simulating: '正在模拟',
 			busy: '忙',
-			changingTool: '改变工具',
+			changingTool: '改变喷头',
 			idle: '空闲',
 			unknown: '未知'
 		},
@@ -323,7 +338,7 @@ export default {
 		notMounted: '没有装载',
 		extracting: '正在提取',
 		uploading: '正在上传',
-		active: '活动',
+		active: '运行',
 		standby: '待机'
 	},
 	input: {
@@ -335,20 +350,19 @@ export default {
 		addRPM: '新的转速值'
 	},
 	jobProgress: {
-		simulating: '正在模拟{0}, {1} 完成',
-		simulated: '已模拟{0}, 100 % 完成',
-		processing: '正在处理{0}, {1}完成',
-		processed: '已处理{0}, 100 % 完成',
-		printing: '正在打印{0}, {1}完成',
-		printed: '已打印{0}, 100 %完成',
+		simulating: '正在模拟{0}, 已完成{1}',
+		simulated: '已模拟{0}, 已完成100%',
+		processing: '正在处理{0}, 已完成{1}',
+		processed: '已处理{0}, 已完成100%',
+		printing: '正在打印{0}, 已完成{1}',
+		printed: '已打印{0}, 已完成100%',
 		noJob: '没有任务在运行.',
 		layer: '第{0}层，共{1}层',
-		filament: '细丝用量 {0}',
-		filamentRemaining: '{0}剩余'
+		filament: '已用耗材{0}',
+		filamentRemaining: '剩余{0}'
 	},
 	list: {
 		baseFileList: {
-			driveUnmounted: '驱动器未装载',
 			fileName: '文件名',
 			size: '大小',
 			lastModified: '最后修改',
@@ -358,6 +372,7 @@ export default {
 			delete: '删除',
 			downloadZIP: '下载为ZIP',
 			noFiles: '没有文件或者目录',
+      driveUnmounted: '驱动器未装载',
 			goUp: '转到顶部'
 		},
 		menu: {
@@ -373,7 +388,7 @@ export default {
 			downloadCSV: '下载为CSV格式'
 		},
 		filament: {
-			noFilaments: '没有细丝'
+			noFilaments: '没有耗材'
 		},
 		macro: {
 			caption: '宏',
@@ -384,14 +399,14 @@ export default {
 		jobs: {
 			height: '对象高度',
 			layerHeight: '层高度',
-			filament: '细丝用量',
+			filament: '耗材用量',
 			printTime: '打印时间',
 			simulatedTime: '模拟时间',
-			generatedBy: '生成软件',
-
-			noJobs: '没有任务',
-			start: '开始任务文件',
-			simulate: '模拟文件'
+			generatedBy: '切片软件',
+			
+			noJobs: '没有任务文件',
+			start: '开始打印',
+			simulate: '模拟打印'
 		},
 		system: {
 			noFiles: '没有系统文件',
@@ -403,35 +418,40 @@ export default {
 			caption: '机器控制',
 			dashboard: '仪表板',
 			console: '控制台',
-			heightmap: '高度图'
 		},
 		job: {
 			caption: '当前任务',
 			status: '状态',
 			webcam: '摄像头',
-			visualizer: '显示'
 		},
 		files: {
 			caption: '文件管理',
 			jobs: '任务',
-			filaments: '细丝',
+			filaments: '耗材',
 			macros: '宏',
 			menu: '显示',
 			system: '系统',
 			web: '网页'
 		},
+		plugins: {
+			caption: '插件'
+		},
 		settings: {
-			caption: '设置',
-			general: '常规',
-			machine: '机器特性',
-			update: '更新'
+			caption: '系统设置',
+			general: '常规设置',
+			machine: '机器设置',
 		}
 	},
 	notification: {
 		compress: {
 			title: '正在压缩文件...',
-			message: '你的文件已经开始压缩，请等待...',
+			message: '文件已经开始压缩，请稍后...',
 			errorTitle: '压缩文件失败'
+		},
+		decompress: {
+			title: '解压文件...',
+			message: '正在解压文件，请稍后... ',
+			errorTitle: '解压文件失败'
 		},
 		delete: {
 			errorTitle: '删除{0}失败',
@@ -440,15 +460,14 @@ export default {
 			successMultiple: '已成功删除{0}个项目'
 		},
 		deleteFilament: {
-			errorTitle: '删除细丝失败',
-			errorStillLoaded: '仍然装载至少一根选定的细丝。 请在继续之前卸载它',
-			errorSubDirectories: '细丝{0}包含子目录。 请手动删除它们，然后重试。'
+			errorTitle: '删除耗材失败',
+			errorStillLoaded: '至少一个选定的耗材被装载。 请在继续之前卸载它',
+			errorSubDirectories: '耗材{0}包含子目录。 请手动删除它们，然后重试。'
 		},
 		download: {
-			title: '正在下载{0} @ {1}, {2}%完成',
-			message: '文件已经开始下载，请等待...',
+			title: '正在下载{0} 总共{1}个,已完成{2}%',
+			message: '文件已经开始下载，请稍后...',
 			success: '已成功下载{0}，总计{1}',
-			successMulti: '已成功下载{0}文件',
 			error: '下载{0}失败'
 		},
 		message: '消息',
@@ -459,28 +478,27 @@ export default {
 		newDirectory: {
 			errorTitle: '创建目录失败',
 			successTitle: '目录已创建',
-			successMessage: '创建目录{0}已完成'
+			successMessage: '创建目录{0}成功'
 		},
 		newFilament: {
-			errorTitle: '创建细丝失败',
-			errorTitleMacros: '创建细丝宏失败',
-			successTitle: '细丝已创建',
-			successMessage: '创建细丝{0}已成功'
+			errorTitle: '创建耗材失败',
+			errorTitleMacros: '创建耗材宏失败',
+			successTitle: '耗材已创建',
+			successMessage: '创建耗材{0}成功'
 		},
 		rename: {
 			success: '成功重命名{0}为{1}',
 			error: '重命名{0}为{1}失败',
 		},
 		renameFilament: {
-			errorTitle: '重命名细丝失败',
-			errorStillLoaded: '选定的细丝已装载。请在继续之前卸载它'
+			errorTitle: '重命名耗材失败',
+			errorStillLoaded: '选定的耗材已装载。请在继续之前卸载它'
 		},
 		responseTooLong: '响应时间过长，请查看控制台',
 		upload: {
-			title: '正在上传{0} @ {1}, {2}%完成',
-			message: '文件已经开始上传，请等待...',
+			title: '正在上传{0} 总共{1}个,已完成{2}%',
+			message: '文件已经开始上传，请稍后...',
 			success: '已成功上传{0}，总计{1}',
-			successMulti: '已成功上传{0}文件',
 			error: '上传{0}失败'
 		}
 	},
@@ -491,34 +509,34 @@ export default {
 			off: '关'
 		},
 		babystepping: {
-			caption: 'Z轴微步调整',
+			caption: 'Z轴偏移',
 			current: '当前偏移: {0}'
 		},
 		extrude: {
-			caption: '挤出机控制',
+			caption: '挤出控制',
 			mix: '混合',
 			mixRatio: '混合比例：',
-			amount: '进料量{0}:',
-			feedrate: '进给速度{0}:',
+			amount: '进料长度{0}:',
+			feedrate: '移动速度{0}:',
 			retract: '回退',
 			extrude: '挤出'
 		},
 		extrusionFactors: {
-			caption: '挤出机系数',
+			caption: '挤出倍率',
 			changeVisibility: '改变可见性',
 			extruder: '挤出机{0}',
 			noExtruders: '没有挤出机'
 		},
 		fan: {
 			caption: '风扇控制',
-			selection: '风扇选择：',
-			toolFan: '工具风扇',
+			selection: '选择风扇：',
+			toolFan: '喷头风扇',
 			fan: '风扇{0}'
 		},
 		fans: {
 			caption: '风扇',
 			changeVisibility: '改变可见性',
-			toolFan: '工具风扇',
+			toolFan: '喷头风扇',
 			fan: '风扇{0}',
 			noFans: '没有风扇'
 		},
@@ -547,7 +565,7 @@ export default {
 		},
 		jobEstimations: {
 			caption: '估算',
-			filament: '细丝用量',
+			filament: '耗材用量',
 			file: '文件进度',
 			layer: '层时间',
 			slicer: '切片',
@@ -557,28 +575,28 @@ export default {
 			caption: '任务信息',
 			height: '高度:',
 			layerHeight: '层高:',
-			filament: '细丝用量:',
-			generatedBy: '生成软件:'
+			filament: '耗材用量:',
+			generatedBy: '切片软件:'
 		},
 		movement: {
-			caption: '机器移动',
-			compensation: '补偿和校准',
-			runBed: '真正的床铺平整(G32)',
-			runDelta: 'Delta校准(G32)',
-			compensationInUse: '使用补偿: {0}',
-			disableBedCompensation: '禁用床补偿(M561)',
+			caption: '机器控制',
+			compensation: '调平和补偿',
+			runBed: '热床调平(G32独立多Z轴机器)',
+			runDelta: '三角洲机器调平(G32)',
+			compensationInUse: '使用补偿:{0}',
+			disableBedCompensation: '禁用热床调平(M561)',
 			disableMeshCompensation: '禁用网格补偿(G29 S2)',
 			editMesh: '定义网格补偿区域(M557)',
 			runMesh: '运行网格补偿(G29)',
 			loadMesh: '从SD卡加载已保存的高度图(G29 S1)',
-			axesNotHomed: '以下轴未归位：|以下轴未归位：',
+			axesNotHomed: '以下轴未归零：|以下轴未归零：',
 			noAxes: '没有轴'
 		},
 		settingsAbout: {
 			caption: '关于',
 			developedBy: 'Web界面开发：',
 			for: ' 适用于：',
-			licensedUnder: '中文翻译：宁甲尊  许可条款：'
+			licensedUnder: '中文翻译：宁甲尊 | 现维护：'
 		},
 		settingsAppearance: {
 			caption: '外观',
@@ -592,6 +610,7 @@ export default {
 		settingsCommunication: {
 			caption: '通讯',
 			pingInterval: '空闲时的PING间隔（ms）',
+			updateDelay: '更新延迟(ms)',
 			ajaxRetries: '最大AJAX重试次数',
 			updateInterval: '更新间隔（{0}）',
 			extendedUpdateEvery: '扩展状态更新间隔',
@@ -600,12 +619,17 @@ export default {
 			unavailable: '没有可用的设置'
 		},
 		settingsElectronics: {
-			caption: '电子器件',
+			caption: '硬件信息',
 			diagnostics: '诊断',
 			board: '主板：{0}',
 			firmware: '固件：{0} ({1})',
 			dwsFirmware: 'Duet WiFi Server版本：{0}',
 			updateNote: '注意：您可以在“系统”页面上安装更新。'
+		},
+		settingsEndstops: {
+			caption: '限位开关',
+			index: '序号',
+			triggered: '触发'
 		},
 		settingsGeneral: {
 			caption: '常规',
@@ -615,22 +639,17 @@ export default {
 			cacheStorageLocal: '将缓存保存在本地存储中',
 			cacheSaveDelay: '缓存更改的更新延迟（{0}）'
 		},
-		settingsEndstops: {
-			caption: '限位开关',
-			index: '序号',
-			triggered: '触发'
-		},
 		settingsListItems: {
 			caption: '列表项目',
-			toolTemperatures: '工具温度',
+			toolTemperatures: '喷头温度',
 			bedTemperatures: '热床温度',
 			chamberTemperatures: '加热室温度',
 			spindleRPM: '轴转速'
 		},
 		settingsMachine: {
-			caption: '机器特性',
-			babystepAmount: 'Z轴每个微调步进距离（{0}）',
-			moveFeedrate: '移动按钮的进给速度（{0}）'
+			caption: '机器设置',
+			babystepAmount: 'Z轴微调步进距离（{0}）',
+			moveFeedrate: '机器移动进给速度（{0}）'
 		},
 		settingsNotifications: {
 			caption: '通知',
@@ -646,7 +665,7 @@ export default {
 			webcamEmbedded: '在iframe中嵌入网络摄像头图像',
 			webcamRotation: '旋转网络摄像头图像',
 			webcamFlip: '翻转网络摄像头图像',
-			flipNone: '没有',
+			flipNone: '无',
 			flipX: '翻转X',
 			flipY: '翻转Y',
 			flipBoth: '翻转两者'
@@ -657,14 +676,14 @@ export default {
 		status: {
 			caption: '状态',
 			mode: '模式：{0}',
-			toolPosition: '工具位置',
-			machinePosition: '机器位置',
-			extruders: '挤出机驱动器',
-			extruderDrive: '驱动器{0}',
-			speeds: '速度',
+			toolPosition: '喷头坐标',
+			machinePosition: '机器坐标',
+			extruders: '挤出数值',
+			extruderDrive: 'E{0}',
+			speeds: '运行速度',
 			requestedSpeed: '请求速度',
 			topSpeed: '最高速度',
-			sensors: '传感器',
+			sensors: '传感元件',
 			mcuTemp: 'MCU温度',
 			minMax: '最小值：{0}，最大值：{1}',
 			vIn: '输入电压',
@@ -677,15 +696,15 @@ export default {
 			caption: '工具',
 			controlAll: '控制所有',
 			turnEverythingOff: '关闭所有',
-			allActiveTemperatures: '设置所有活动温度',
+			allActiveTemperatures: '设置所有运行温度',
 			allStandbyTemperatures: '设置所有待机温度',
-			tool: '工具{0}',
-			loadFilament: '载入细丝',
-			changeFilament: '更改细丝',
-			unloadFilament: '卸载细丝',
+			tool: '喷头{0}',
+			loadFilament: '加载耗材',
+			changeFilament: '更改耗材',
+			unloadFilament: '卸载耗材',
 			heater: '加热器{0}',
-			current: '当前',
-			active: '活动',
+			current: '温度',
+			active: '运行',
 			standby: '待机',
 			bed: '热床{0}',
 			chamber: '加热室{0}',
@@ -697,7 +716,7 @@ export default {
 				showInChart: '在图表中显示',
 				noItems: '没有其它加热器'
 			},
-			noTools: '没有工具'
+			noTools: '没有喷头'
 		},
 		webcam: {
 			caption: '网络摄像头监控',
@@ -705,23 +724,144 @@ export default {
 		}
 	},
 	plugins: {
+		autoUpdate: {
+			menuCaption: '更新'
+		},
+		gcodeViewer: {
+			caption: 'G代码查看器',
+			view3D: '3D 视图',
+			fullscreen: '全屏',
+			showConfiguration: '显示查看器配置',
+			resetCamera: {
+				caption : '重置视图',
+				title: '将视图重置到默认位置'
+			},
+			cancelLoad: '取消文件加载',
+			reloadView: { 
+				caption : '重新加载视图',
+				title : '重新加载当前的G代码，在更改颜色，进给率颜色等设置时使用。'
+			},
+			loadCurrentJob:  { 
+				caption :'加载当前任务',  
+				title : '加载当前的打印或模拟任务'
+			},
+			unloadGCode:  { 
+				caption: '关闭当前文件',
+				title : '从查看器中删除已加载的G代码'
+			},
+			loadLocalGCode: { 
+				caption : '打开本地文件',
+				title : '从电脑硬盘加载到查看器中'
+			},
+			showCursor: '显示喷嘴',
+			showTravels: '显示路径',
+			renderQuality: {
+				caption : '显示质量',
+				title : '调整查看器的可视化质量。 级别越高，可用的顶点和渲染模式越多'
+			},
+			sbc: 'SBC',
+			low: '低',
+			medium: '中',			
+			high: '高',
+			ultra: '极好',
+			max: '最高',
+			forceLineRendering: '强制线渲染',
+			transparency: '透明度',
+			showSolid: '显示实心',
+			spreadLines: '展开层',
+			extruders: {
+				caption:  '挤出设置',
+				title : '设置用于渲染挤出机的颜色'
+			},
+			tool: '喷头 {0}',
+			resetColor: '重置喷头颜色 | 重置喷头颜色',
+			renderMode : {
+				caption : '显示模式 | 显示模式',
+				title : '渲染模式使您可以在查看器中设置挤出机颜色或进给率线颜色'
+			},
+			color: '颜色',
+			feedrate: '移动速度',
+			minFeedrate: '最小移动速度 (mm/s)',
+			maxFeedrate: '最大移动速度 (mm/s)',
+			minFeedrateColor: '最小移动速度颜色',
+			maxFeedrateColor: '最大移动速度颜色',
+			progress: {
+				caption : '显示范围',
+				title: '设置打印颜色以进行进度跟踪'
+			},
+			topClipping: '顶部裁剪',
+			bottomClipping:'底部裁剪',
+			progressColor: '进度颜色',
+			liveZTracking: '实时Z跟踪',
+			settings: '其他设置',
+			background: '背景颜色',
+			bedRenderMode:  '热床显示模式',
+			bed: '热床',
+			volume: '体积',
+			showAxes: '显示轴',
+			showObjectLabels: '显示对象标签',
+			cameraInertia: '平滑移动',
+			showObjectSelection: {
+				caption : '显示对象选择',
+				title : '如果可以在当前打印中检测到对象，则启用'
+			},
+			renderFailed: '先前的渲染失败。 将渲染质量设置为SBC',
+		},
 		heightmap: {
+			menuCaption: '高度图',
+			listTitle: '高度图',
+			none: '无数据',
 			scale: '比例：',
 			orMore: '更大',
 			orLess: '更小',
 			axes: '轴：',
 			notAvailable: '高度图无效',
+			statistics: '统计',
 			numPoints: '点数: {0}',
 			radius: '探测半径: {0}',
 			area: '探测面积: {0}',
 			maxDeviations: '最大偏差: {0} / {1}',
 			meanError: '平均误差: {0}',
 			rmsError: '均方误差: {0}',
-			topView: '顶视图',
+			display: '显示',
 			colorScheme: '配色方案:',
 			terrain: '地形',
 			heat: '热力',
-			reload: '重载高度图'
+			invertZ: '反转Z座标',
+			topView: '顶视图',
+		},
+		objectModelBrowser: {
+			menuCaption: '对象模型'
+		}
+	},
+	tabs: {
+		generalSettings: {
+			caption: '一般设置'
+		},
+		machineSettings: {
+			caption: '一般设置'
+		},
+		plugins: {
+			caption: '插件',
+			headers: {
+				name: '名称',
+				author: '作者',
+				version: '版本',
+				license: '许可',
+				components: '组件',
+				dependencies: '依赖',
+				status: '状态'
+			},
+			optional: '可选的',
+			start: '启动',
+			partiallyStarted: '部分开始',
+			started: '已启动',
+			stop: '停止',
+			deactivated: '停用',
+			stopped: '已停止',
+			uninstall: '卸载',
+			noPlugins: '没有插件',
+			refreshNote: '刷新页面以完成一些DWC插件的卸载'
 		}
 	}
 }
